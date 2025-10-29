c465cfa243940ca3f28edf9934aaf62ef9169e7d
zakar <elhaiki.zakaria.studente@itispaleocapa.it>
2025-10-29 21:32:24 +0100

    creati gli attributi e impostazione del main

---
+++ src/Mattoncino.java
@@ -0,0 +1,14 @@
+public class Mattoncino {
+    private int codiceunivoco;
+    private float peso;
+    private int red;
+    private int blue;
+    private int green;
+    private int larghezza;
+    private int lunghezza;
+    private int altezza;
+
+
+
+
+}
